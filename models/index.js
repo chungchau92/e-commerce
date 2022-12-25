0cb19152a829bf7317a20dec8096a3067cf6c6ee
chungchau92 <chungchau92@gmail.com>
2022-12-25 22:33:25 +0700

    add category_routes

--- models/index.js
+++ models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: "product_tag",
+  }
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: "product_tag",
+  }
+})
 
 module.exports = {
   Product,
